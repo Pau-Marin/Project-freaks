0cab861521d3febda1748c2b9b39d312f80f727c
Pau Marin <6287958+Pau-Marin@users.noreply.github.com>
2023-01-11 00:43:15 +0100

    Cambio a NextJs

---
+++ web/app/page.jsx
@@ -0,0 +1,20 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec turpis
+        odio, varius eget enim id, laoreet blandit urna. Sed eros lorem, congue
+        ac mollis at, imperdiet ac ipsum. Nullam rhoncus et leo quis eleifend.
+        Quisque sed facilisis lacus. Pellentesque ipsum lorem, ultrices eu elit
+        non, finibus pulvinar augue. Sed sit amet tellus in dui consectetur
+        pretium. Ut lacinia est ut interdum cursus. Maecenas nec nunc turpis.
+        Duis porta ac orci in finibus. Donec a cursus nisi. Duis elit dolor,
+        eleifend vel nisi eu, dictum varius enim. Integer semper, nibh vel
+        dictum pulvinar, enim urna egestas nisi, eu scelerisque massa eros sed
+        quam. Praesent nec suscipit neque. Vestibulum mattis magna urna, sed
+        dapibus ex elementum eu.
+      </p>
+    </div>
+  )
+}
